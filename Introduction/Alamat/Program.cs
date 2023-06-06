2b8ba9ad47aeb60ad496c483e2216d738013704e
Dimas Wisnu Saputro <Dimaswisnu2500@gmail.com>
2023-06-06 15:15:06 +0700

    Belajar Bahasa C#

---
+++ Introduction/Alamat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Alamat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? name = null;
+            string? address = null;
+            //int? age = null;
+            Console.Write("Masukkan Nama Anda: ");
+            name = Console.ReadLine();
+
+            Console.Write("Masukkan Alamat Rumah Anda: ");
+            address = Console.ReadLine();
+
+            Console.Write("Berapakah Usia Anda: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Nama Anda Adalah " + name + "\n" +"Rumah Anda berada di " + address + "\n" + "Umur anda adalah " + age);
+        }
+    }
+}
---
+++ Introduction/Data Diri/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataDiri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? firstName = null;
+            string? lastName = null;
+            Console.Write("Masukkan Nama Depan Anda: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Masukkan Nama Belakang Anda: ");
+            lastName = Console.ReadLine();
+
+            if (firstName != null && lastName != null)
+            {
+                Console.WriteLine("Nama Anda Adalah " + firstName + " " + lastName); // Use the variable
+            }
+        }
+    }
+}
